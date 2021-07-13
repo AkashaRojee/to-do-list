26958f5aa6056ea64810fe2cad9bf673b62731d4
Akasha <Akasha.Rojee@gmail.com>
2021-07-13 23:11:33 +0400

    Iterate over tasks array and populate list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './style.css';
 
+const list = document.querySelector('ul');
+
+function createElement(elementType, classNames = '', attributes = {}, innerHTML = '') {
+  const elementObject = document.createElement(elementType);
+  if (classNames) elementObject.classList.add(...(classNames.split(' ')));
+  Object.keys(attributes).forEach((attribute) => {
+    elementObject.setAttribute(attribute, attributes[attribute]);
+  });
+  elementObject.innerHTML = innerHTML;
+  return elementObject;
+}
+
+function appendListItem(description) {
+  list.appendChild(createElement('li', '', {}, description));
+}
+
 class Task {
   constructor(description, completed, index) {
     this.description = description;
@@ -17,6 +33,10 @@ class ToDoList {
   add(description, completed, index) {
     this.tasks.push(new Task(description, completed, index));
   }
+
+  populate() {
+    this.tasks.map(task => task.description).forEach(description => appendListItem(description));
+  }
 }
 
 let toDoList = new ToDoList();
@@ -27,4 +47,4 @@ toDoList.add('Task3', false, 2);
 toDoList.add('Task4', false, 3);
 toDoList.add('Task5', false, 4);
 
-console.log(toDoList);
+toDoList.populate();
