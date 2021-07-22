695f596a8f9ffe063c1a3a2705f42a50af6df7ee
Hubert MUGABO <mugberto@gmail.com>
2021-07-22 19:47:28 +0200

    Test ToDoList.add() when tasks array is not empty

--- src/js/ToDoList.test.js
+++ src/js/ToDoList.test.js
@@ -10,6 +10,13 @@ test('add() adds new task to empty tasks array', () => {
   expect(result.tasks).toStrictEqual(expected);
 });
 
-// test('add() adds new task to non-empty tasks array', () => {
+test('add() adds new task to non-empty tasks array', () => {
+  let expected = [new Task('Task 1', false, 0), new Task('Task 2', false, 1)];
+
+  let result = new ToDoList();
+  result.tasks.push(new Task('Task 1', false, 0));
+  result.add('Task 2', false, 1);
+
+  expect(result.tasks).toStrictEqual(expected);
   
-// });
+});
