bac8b38fd551192ff9b3758d347ba78fbd4d7f10
Akasha <Akasha.Rojee@gmail.com>
2021-07-22 19:36:26 +0400

    Complete test for ToDoList.add when tasks array is empty

--- src/js/ToDoList.test.js
+++ src/js/ToDoList.test.js
@@ -2,23 +2,15 @@ import ToDoList from './ToDoList.js';
 import Task from './Task.js';
 
 test('add() adds new task to empty tasks array', () => {
-  let expected = new ToDoList();
-  expected.tasks = [new Task('Task 1', false, 0)];
-  expected.list = new DOMParser().parseFromString(
+  document.body.innerHTML =
     '<ul>' +
-    '</ul>',
-    'text/html'
-  ).querySelector('ul');
-  expected.listItems = new DOMParser().parseFromString(
-    '',
-    'text/html'
-  ).querySelector('li');
-  // expected.ls = new LocalStorageMock();
+    '</ul>';
+  let expected = [new Task('Task 1', false, 0)];
 
   let result = new ToDoList();
   result.add('Task 1', false, 0);
 
-  expect(result.tasks).toStrictEqual(expected.tasks);
+  expect(result.tasks).toStrictEqual(expected);
 });
 
 // test('add() adds new task to non-empty tasks array', () => {
