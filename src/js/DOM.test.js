27ac19d723ce8170e3e8af493e9c2a5278ece030
Akasha <Akasha.Rojee@gmail.com>
2021-07-22 13:56:54 +0400

    Add test for DOM.queryElement

---
+++ src/js/DOM.test.js
@@ -0,0 +1,24 @@
+import { queryElement } from './DOM.js'
+
+test('Query selector for single element returns element', () => {
+  //Set up document body
+  document.body.innerHTML =
+    '<ul>' +
+    ' <li>Task 1</li>' +
+    ' <li>Task 2</li>' +
+    ' <li>Task 3</li>' +
+    '</ul>';
+  const expected = new DOMParser().parseFromString(
+    '<ul>' +
+    ' <li>Task 1</li>' +
+    ' <li>Task 2</li>' +
+    ' <li>Task 3</li>' +
+    '</ul>',
+    'text/html'
+  ).querySelector('ul');
+
+  const result = queryElement(document, 'ul');
+
+  expect(result).toStrictEqual(expected);
+
+});
