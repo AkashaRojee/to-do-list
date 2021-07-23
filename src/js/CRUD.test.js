84d0ef07b157622172cb2183ca92a4859e86ca4a
Akasha <Akasha.Rojee@gmail.com>
2021-07-23 18:43:53 +0400

    Add tests for edit description

--- src/js/CRUD.test.js
+++ src/js/CRUD.test.js
@@ -9,8 +9,8 @@ import SuperArray from './SuperArray.js';
 let e; let spanArray; let toDoList; let checkboxList; let drag; let
   crud;
 
-function setupObjects() {
-  e = new Event('click');
+function setupObjects(eventType = 'click') {
+  e = new Event(eventType);
   toDoList = new ToDoList();
   checkboxList = new CheckboxList();
   drag = new Drag();
@@ -404,54 +404,54 @@ describe('delete', () => {
 
 });
 
-describe.only('Edit', () => {
+describe.only('edit', () => {
 
-  test('EditDescription',() => {
+  beforeEach(() => {
 
-    document.body.innerHTML = '<div class="flex-row">'
-        + ' <input type="text" value="Task 1" class="fill" placeholder="Add to your list...">'
-        + ' <button type="button" class="material-icons">add</button>'
-        + '</div>'
-        + '<ul>'
-        + ' <li class="flex-row space-between align-center" draggable="true">'
-        + '   <div class="flex-row align-center fill">'
-        + '     <input type="checkbox">'
-        + '     <span class="fill" contenteditable="true">Task 1</span>'
-        + '   </div>'
-        + '   <button class="material-icons drag" type="button">more_vert</button>'
-        + ' </li>'
-        + ' <li class="flex-row space-between align-center" draggable="true">'
-        + '   <div class="flex-row align-center fill">'
-        + '     <input type="checkbox">'
-        + '     <span class="fill" contenteditable="true">Task 2</span>'
-        + '   </div>'
-        + '   <button class="material-icons drag" type="button">more_vert</button>'
-        + ' </li>'
-        + ' <li class="flex-row space-between align-center" draggable="true">'
-        + '   <div class="flex-row align-center fill">'
-        + '     <input type="checkbox">'
-        + '     <span class="fill" contenteditable="true">Task 3(edited)</span>'
-        + '   </div>'
-        + '   <button class="material-icons drag" type="button">more_vert</button>'
-        + ' </li>'
-        + ' <li class="flex-row space-between align-center" draggable="true">'
-        + '   <div class="flex-row align-center fill">'
-        + '     <input type="checkbox">'
-        + '     <span class="fill" contenteditable="true">Task 4</span>'
-        + '   </div>'
-        + '   <button class="material-icons drag" type="button">more_vert</button>'
-        + ' </li>'
-        + ' <li class="flex-row space-between align-center" draggable="true">'
-        + '   <div class="flex-row align-center fill">'
-        + '     <input type="checkbox">'
-        + '     <span class="fill" contenteditable="true">Task 5</span>'
-        + '   </div>'
-        + '   <button class="material-icons drag" type="button">more_vert</button>'
-        + ' </li>'
-        + '</ul>'
-        + '<div class="flex-row justify-center align-center">'
-        + ' <button type="button" class="btn-clear pointer">Clear all completed</button>'
-        + '</div>';
+    document.body.innerHTML = ('<div class="flex-row">'
+    + ' <input type="text" value="Task 1" class="fill" placeholder="Add to your list...">'
+    + ' <button type="button" class="material-icons">add</button>'
+    + '</div>'
+    + '<ul>'
+    + ' <li class="flex-row space-between align-center" draggable="true">'
+    + '   <div class="flex-row align-center fill">'
+    + '     <input type="checkbox">'
+    + '     <span class="fill" contenteditable="true">Task 1</span>'
+    + '   </div>'
+    + '   <button class="material-icons drag" type="button">more_vert</button>'
+    + ' </li>'
+    + ' <li class="flex-row space-between align-center" draggable="true">'
+    + '   <div class="flex-row align-center fill">'
+    + '     <input type="checkbox">'
+    + '     <span class="fill" contenteditable="true">Task 2</span>'
+    + '   </div>'
+    + '   <button class="material-icons drag" type="button">more_vert</button>'
+    + ' </li>'
+    + ' <li class="flex-row space-between align-center" draggable="true">'
+    + '   <div class="flex-row align-center fill">'
+    + '     <input type="checkbox">'
+    + '     <span class="fill" contenteditable="true">Task three</span>'
+    + '   </div>'
+    + '   <button class="material-icons drag pointer" type="button">delete</button>'
+    + ' </li>'
+    + ' <li class="flex-row space-between align-center" draggable="true">'
+    + '   <div class="flex-row align-center fill">'
+    + '     <input type="checkbox">'
+    + '     <span class="fill" contenteditable="true">Task 4</span>'
+    + '   </div>'
+    + '   <button class="material-icons drag" type="button">more_vert</button>'
+    + ' </li>'
+    + ' <li class="flex-row space-between align-center" draggable="true">'
+    + '   <div class="flex-row align-center fill">'
+    + '     <input type="checkbox">'
+    + '     <span class="fill" contenteditable="true">Task 5</span>'
+    + '   </div>'
+    + '   <button class="material-icons drag" type="button">more_vert</button>'
+    + ' </li>'
+    + '</ul>'
+    + '<div class="flex-row justify-center align-center">'
+    + ' <button type="button" class="btn-clear pointer">Clear all completed</button>'
+    + '</div>').replace(/>\s+</g, '><');
 
     setupObjects();
 
@@ -467,18 +467,86 @@ describe.only('Edit', () => {
       new Task('Task 5', false, 5),
     ];
 
+  });
+
+  test('Edit item updates array',() => {
+
     const expected = [
       new Task('Task 1', false, 1),
       new Task('Task 2', false, 2),
-      new Task('Task 3(edited)', false, 3),
+      new Task('Task three', false, 3),
       new Task('Task 4', false, 4),
       new Task('Task 5', false, 5),
     ];
 
     crud.editDescription(e, spanArray, toDoList);
-
-    const result = toDoList.tasksp;
+    const result = toDoList.tasks;
 
     expect(result).toStrictEqual(expected);
+    
+  })
+
+  test('Edit item updates list in DOM', () => {
+
+    const expected = (
+      '<li class="flex-row space-between align-center" draggable="true">'
+      + '  <div class="flex-row align-center fill">'
+      + '    <input type="checkbox">'
+      + '    <span class="fill" contenteditable="true">Task 1</span>'
+      + '  </div>'
+      + '  <button class="material-icons drag" type="button">more_vert</button>'
+      + '</li>'
+      + '<li class="flex-row space-between align-center" draggable="true">'
+      + '  <div class="flex-row align-center fill">'
+      + '    <input type="checkbox">'
+      + '    <span class="fill" contenteditable="true">Task 2</span>'
+      + '  </div>'
+      + '  <button class="material-icons drag" type="button">more_vert</button>'
+      + '</li>'
+      + '<li class="flex-row space-between align-center" draggable="true">'
+      + '  <div class="flex-row align-center fill">'
+      + '    <input type="checkbox">'
+      + '    <span class="fill" contenteditable="true">Task three</span>'
+      + '  </div>'
+      + '  <button class="material-icons drag" type="button">more_vert</button>'
+      + '</li>'
+      + '<li class="flex-row space-between align-center" draggable="true">'
+      + '  <div class="flex-row align-center fill">'
+      + '    <input type="checkbox">'
+      + '    <span class="fill" contenteditable="true">Task 4</span>'
+      + '  </div>'
+      + '  <button class="material-icons drag" type="button">more_vert</button>'
+      + '</li>'
+      + '<li class="flex-row space-between align-center" draggable="true">'
+      + '   <div class="flex-row align-center fill">'
+      + '    <input type="checkbox">'
+      + '    <span class="fill" contenteditable="true">Task 5</span>'
+      + '  </div>'
+      + '  <button class="material-icons drag" type="button">more_vert</button>'
+      + '</li>'
+    ).replace(/>\s+</g, '><');
+
+    crud.editDescription(e, spanArray, toDoList);
+    const result = document.querySelector('ul').innerHTML;
+
+    expect(result).toStrictEqual(expected);
+
+  });
+
+  test('Edit item updates local storage',() => {
+
+    const expected = [
+      { description: 'Task 1', completed: false, index: 1 },
+      { description: 'Task 2', completed: false, index: 2 },
+      { description: 'Task three', completed: false, index: 3 },
+      { description: 'Task 4', completed: false, index: 4 },
+      { description: 'Task 5', completed: false, index: 5 },
+    ];
+
+    crud.editDescription(e, spanArray, toDoList);
+    const result = JSON.parse(localStorage.getItem('tasks'));
+
+    expect(result).toStrictEqual(expected);
+
   })
 });
