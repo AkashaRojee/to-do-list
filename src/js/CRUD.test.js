6e7c968aab2649320dea7f9e3b72eb973f1116df
Hubert MUGABO <mugberto@gmail.com>
2021-07-23 10:41:25 +0200

    Test editDescription method

--- src/js/CRUD.test.js
+++ src/js/CRUD.test.js
@@ -401,4 +401,84 @@ describe('delete', () => {
       expect(result).toStrictEqual(expected);
     });
   });
+
+});
+
+describe.only('Edit', () => {
+
+  test('EditDescription',() => {
+
+    document.body.innerHTML = '<div class="flex-row">'
+        + ' <input type="text" value="Task 1" class="fill" placeholder="Add to your list...">'
+        + ' <button type="button" class="material-icons">add</button>'
+        + '</div>'
+        + '<ul>'
+        + ' <li class="flex-row space-between align-center" draggable="true">'
+        + '   <div class="flex-row align-center fill">'
+        + '     <input type="checkbox">'
+        + '     <span class="fill" contenteditable="true">Task 1</span>'
+        + '   </div>'
+        + '   <button class="material-icons drag" type="button">more_vert</button>'
+        + ' </li>'
+        + ' <li class="flex-row space-between align-center" draggable="true">'
+        + '   <div class="flex-row align-center fill">'
+        + '     <input type="checkbox">'
+        + '     <span class="fill" contenteditable="true">Task 2</span>'
+        + '   </div>'
+        + '   <button class="material-icons drag" type="button">more_vert</button>'
+        + ' </li>'
+        + ' <li class="flex-row space-between align-center" draggable="true">'
+        + '   <div class="flex-row align-center fill">'
+        + '     <input type="checkbox">'
+        + '     <span class="fill" contenteditable="true">Task 3(edited)</span>'
+        + '   </div>'
+        + '   <button class="material-icons drag" type="button">more_vert</button>'
+        + ' </li>'
+        + ' <li class="flex-row space-between align-center" draggable="true">'
+        + '   <div class="flex-row align-center fill">'
+        + '     <input type="checkbox">'
+        + '     <span class="fill" contenteditable="true">Task 4</span>'
+        + '   </div>'
+        + '   <button class="material-icons drag" type="button">more_vert</button>'
+        + ' </li>'
+        + ' <li class="flex-row space-between align-center" draggable="true">'
+        + '   <div class="flex-row align-center fill">'
+        + '     <input type="checkbox">'
+        + '     <span class="fill" contenteditable="true">Task 5</span>'
+        + '   </div>'
+        + '   <button class="material-icons drag" type="button">more_vert</button>'
+        + ' </li>'
+        + '</ul>'
+        + '<div class="flex-row justify-center align-center">'
+        + ' <button type="button" class="btn-clear pointer">Clear all completed</button>'
+        + '</div>';
+
+    setupObjects();
+
+    spanArray = new SuperArray(...document.querySelectorAll('span'));
+    const span = spanArray[2];
+    span.dispatchEvent(e);
+
+    toDoList.tasks = [
+      new Task('Task 1', false, 1),
+      new Task('Task 2', false, 2),
+      new Task('Task 3', false, 3),
+      new Task('Task 4', false, 4),
+      new Task('Task 5', false, 5),
+    ];
+
+    const expected = [
+      new Task('Task 1', false, 1),
+      new Task('Task 2', false, 2),
+      new Task('Task 3(edited)', false, 3),
+      new Task('Task 4', false, 4),
+      new Task('Task 5', false, 5),
+    ];
+
+    crud.editDescription(e, spanArray, toDoList);
+
+    const result = toDoList.tasksp;
+
+    expect(result).toStrictEqual(expected);
+  })
 });
